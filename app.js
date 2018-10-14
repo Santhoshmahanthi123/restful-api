383fbc5217edce9106a9dc93979b59ea6d3ab94d
santhosh <singumahanthisanthosh@gmail.com>
2018-10-14 19:07:15 +0530

    orders folder added and routes added

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
+
 app.use('/products',productRoutes);
+
+app.use('/orders',orderRoutes);
 
 app.listen(port,()=>{
     console.log(`listening to server on ${port} port`);
