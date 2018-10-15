05adbc41935ae605b97f2fcc359d0429bf1d200e
santhosh <singumahanthisanthosh@gmail.com>
2018-10-16 05:13:44 +0530

    Authentication of login route added

--- routes/user.js
+++ routes/user.js
@@ -52,6 +52,40 @@ router.post('/signup',(req,res,next) => {
     });
 });
 
+router.post('/login',(req,res,next) =>{
+
+    User.find({ email : req.body.email})
+    .exec()
+    .then(user =>{
+        if(user.length < 1){
+            return res.status(401).json({
+                message : 'Authorization failed!'
+            });
+        }
+        bcrypt.compare(req.body.password,user[0].password,(err,result) =>{
+            if(err){
+                return res.status(401).json({
+                    message : 'Authorization failed!'
+                });
+            }
+            if(result){
+                return res.status(200).json({
+                    message:'Authentication successful!'
+                });
+            }
+            res.status(401).json({
+                message : 'Authorization failed!'
+            });
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error : err
+        });
+    });
+})
+
 router.delete('/:userId',(req,res,next) => {
     User.remove({_id:req.params.userId})
     .exec()
