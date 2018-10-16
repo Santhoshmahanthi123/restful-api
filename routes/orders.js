9abbfa3fcdcfb3da88d47cd5043cdde2ba55a28a
santhosh <singumahanthisanthosh@gmail.com>
2018-10-16 15:45:21 +0530

    controllers added

--- routes/orders.js
+++ routes/orders.js
@@ -3,39 +3,12 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Order = require('../models/order');
 const Product = require('../models/product');
+const checkAuth = require('../middleware/check-auth');
 
-router.get('/',(req,res,next)=>{
-  Order.find()
-  .select('product quantity _id')
-  .populate('product','name')
-  .exec()
-  .then(docs => {
-      res.status(200).json({
-          count : docs.length,
-          orders : docs.map(doc => {
-              return {
-                    _id: doc._id,
-                    product : doc.product,
-                    quantity : doc.quantity,
-                    request : {
-                        type :'GET',
-                        url :'http://localhost:3000/orders' + doc._id
-                    }
+const OrdersController = require('../controllers/orders')
+router.get('/',checkAuth,OrdersController.orders_get_all);
 
-              }
-          })   
-         
-      });
-
-  })
-  .catch(err=>{
-      res.status(500).json({
-          error : err
-      });
-  })
-});
-
-router.post('/',(req,res,next)=>{
+router.post('/',checkAuth,(req,res,next)=>{
     Product.findById(req.body.productId)
     .then(product => {
         if(!product){
@@ -74,7 +47,7 @@ router.post('/',(req,res,next)=>{
     }); 
 });
 
-router.get('/:orderId',(req,res,next)=>{
+router.get('/:orderId',checkAuth,(req,res,next)=>{
    Order.findById(req.params.orderId)
    .populate('product')
    .select('quantity _id product')
@@ -102,7 +75,7 @@ router.get('/:orderId',(req,res,next)=>{
    })
 });
 
-router.delete('/:orderId',(req,res,next)=>{
+router.delete('/:orderId',checkAuth,(req,res,next)=>{
     const id = req.params.orderId;
    Order.remove({_id : id })
    .exec()
