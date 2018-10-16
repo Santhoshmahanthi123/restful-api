90a9d1b258e3224363a6d2aa73f253e85b6d6ecf
santhosh <singumahanthisanthosh@gmail.com>
2018-10-16 05:33:28 +0530

    setting up json web token

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 //bcrypt hashes the password
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup',(req,res,next) => {
 
@@ -59,22 +60,32 @@ router.post('/login',(req,res,next) =>{
     .then(user =>{
         if(user.length < 1){
             return res.status(401).json({
-                message : 'Authorization failed!'
+                message : 'Authentication failed!'
             });
         }
         bcrypt.compare(req.body.password,user[0].password,(err,result) =>{
             if(err){
                 return res.status(401).json({
-                    message : 'Authorization failed!'
+                    message : 'Authentication failed!'
                 });
             }
             if(result){
+                const token = jwt.sign({
+                    email : user[0].email,
+                    userId : user[0]._id
+                },
+                process.env.JWTKEY,
+                {
+                    expiresIn:"1h"
+                }
+               );
                 return res.status(200).json({
-                    message:'Authentication successful!'
+                    message:'Authentication successful!',
+                    token : token
                 });
             }
             res.status(401).json({
-                message : 'Authorization failed!'
+                message : 'Authentication failed!'
             });
         })
     })
